081d240425eecc6aaf90401b04abc73158896a37
muhamadirfansobis <muhamad.irfan@sobatbisnis.com>
2025-05-06 17:03:39 +0700

    chore(theme): config dynamic theme (#3)
    * chore(theme): config dynamic theme
    
    * chore(theme): update pnpm lock

--- src/components/ui/Wrapper.tsx
+++ src/components/ui/Wrapper.tsx
@@ -3,10 +3,12 @@
 import React, { ReactNode } from 'react';
 import { ThemeProvider } from 'sbg-design-system-alpha';
 import { Footer } from './Footer';
+import { THEME_CONFIG } from '@/themeConfig/themeConfig';
 
 export const Wrapper = ({ children }: { children: ReactNode }) => {
   return (
-    <ThemeProvider themeData="default">
+    // Note: Themeprovider is only use if you want to use custom theme color. If you want use SBG theme color, remove the ThemeProvider
+    <ThemeProvider themeData={THEME_CONFIG}>
       {children}
       <Footer />
     </ThemeProvider>
